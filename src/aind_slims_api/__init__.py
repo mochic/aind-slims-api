5ed1f4cff2f9f6fccf9f81fce9228c1537a4462a
Patrick Latimer <110747402+patricklatimer@users.noreply.github.com>
2024-06-14 15:26:35 -0700

    Generic Client (#10)
    * Initial add
    
    * Update dependencies
    
    * refactor: expand SlimsBaseModel and client
    
    * feat: add mouse model and fetch method
    
    * feat: add user model and fetch method
    
    * Feat client unit tests (#11)
    
    * docs: set line length to 88
    
    * tests: adds test coverage
    
    ---------
    
    Co-authored-by: jtyoung84 <104453205+jtyoung84@users.noreply.github.com>

--- src/aind_slims_api/__init__.py
+++ src/aind_slims_api/__init__.py
@@ -1,2 +1,9 @@
 """Init package"""
+
 __version__ = "0.0.0"
+
+from aind_slims_api.configuration import AindSlimsApiSettings
+
+config = AindSlimsApiSettings()
+
+from aind_slims_api.core import SlimsClient  # noqa
