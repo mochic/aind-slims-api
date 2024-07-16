680ff25ad02181c7022ad4cf69f272fc8a9b0621
Christopher <chrism@alleninstitute.org>
2024-07-16 07:41:16 -0700

    doctest example for model

--- src/aind_slims_api/instrument.py
+++ src/aind_slims_api/instrument.py
@@ -5,13 +5,20 @@ from typing import Any
 
 from pydantic import Field
 
-from aind_slims_api.core import SlimsBaseModel, SlimsClient, SLIMSTABLES
+from aind_slims_api.core import SlimsBaseModel, SLIMSTABLES
 
 logger = logging.getLogger()
 
 
 class SlimsInstrument(SlimsBaseModel):
-    """Model for an instance of the Behavior Session ContentEvent"""
+    """Model for an instance of the Behavior Session ContentEvent
+
+    Examples
+    --------
+    >>> from aind_slims_api.core import SlimsClient
+    >>> client = SlimsClient()
+    >>> instrument = client.fetch_model(SlimsInstrument, nstr_name="323_EPHYS1_OPTO")
+    """
 
     name: str = Field(..., alias="nstr_name")
     pk: int = Field(..., alias="nstr_pk")
@@ -20,47 +27,60 @@ class SlimsInstrument(SlimsBaseModel):
     # todo add more useful fields
 
 
-def fetch_instrument_content(
-    client: SlimsClient,
-    instrument_name: str,
-) -> SlimsInstrument | dict[str, Any] | None:
-    """Fetches behavior sessions for a mouse with labtracks id {mouse_name}
+# def fetch_instrument_content(
+#     client: SlimsClient,
+#     instrument_name: str,
+# ) -> SlimsInstrument | dict[str, Any] | None:
+#     """Fetches behavior sessions for a mouse with labtracks id {mouse_name}
 
-    Returns
-    -------
-    tuple:
-        list:
-            Validated SlimsInstrument objects
-        list:
-            Dictionaries representations of objects that failed validation
+#     Returns
+#     -------
+#     tuple:
+#         list:
+#             Validated SlimsInstrument objects
+#         list:
+#             Dictionaries representations of objects that failed validation
 
-    Notes
-    -----
-    - Todo: add partial name match or some other type of filtering
-    - TODO: reconsider this pattern, consider just returning all records or
-     having number returned be a parameter or setting
-    """
-    validated, unvalidated = client.fetch_models(
-        SlimsInstrument,
-        nstr_name=instrument_name,
+#     Notes
+#     -----
+#     - Todo: add partial name match or some other type of filtering
+#     - TODO: reconsider this pattern, consider just returning all records or
+#      having number returned be a parameter or setting
+#     """
+#     validated, unvalidated = client.fetch_models(
+#         SlimsInstrument,
+#         nstr_name=instrument_name,
+#     )
+#     if len(validated) > 0:
+#         validated_details = validated[0]
+#         if len(validated) > 1:
+#             logger.warning(
+#                 f"Warning, Multiple instruments in SLIMS with name {instrument_name}, "
+#                 f"using pk={validated_details.pk}"
+#             )
+#         return validated_details
+#     else:
+#         if len(unvalidated) > 0:
+#             unvalidated_details = unvalidated[0]
+#             if len(unvalidated) > 1:
+#                 logger.warning(
+#                     "Warning, Multiple instruments in SLIMS with name "
+#                     f"{instrument_name}, "
+#                     f"using pk={unvalidated_details['pk']}"
+#                 )
+#             return unvalidated[0]
+
+#     return None
+
+
+if __name__ == "__main__":
+    import doctest
+    import logging
+
+    logging.basicConfig(level=logging.DEBUG)
+
+    doctest.testmod(
+        optionflags=(
+            doctest.IGNORE_EXCEPTION_DETAIL | doctest.NORMALIZE_WHITESPACE
+        )
     )
-    if len(validated) > 0:
-        validated_details = validated[0]
-        if len(validated) > 1:
-            logger.warning(
-                f"Warning, Multiple instruments in SLIMS with name {instrument_name}, "
-                f"using pk={validated_details.pk}"
-            )
-        return validated_details
-    else:
-        if len(unvalidated) > 0:
-            unvalidated_details = unvalidated[0]
-            if len(unvalidated) > 1:
-                logger.warning(
-                    "Warning, Multiple instruments in SLIMS with name "
-                    f"{instrument_name}, "
-                    f"using pk={unvalidated_details['pk']}"
-                )
-            return unvalidated[0]
-
-    return None
